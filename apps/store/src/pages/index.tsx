7ad8b21e41f2bc1cf6e3034dcd67f0a527a7a87b
Kristian <kristian@coulsy.co.uk>
2022-06-03 20:04:40 +0100

    Got tailwind working for ui package with linting

--- apps/store/src/pages/index.tsx
+++ apps/store/src/pages/index.tsx
@@ -1,3 +1,10 @@
-const Home = () => <div>hello home</div>;
+import { Button } from 'ui';
+
+const Home = () => (
+  <div>
+    <div className="m-5 p-4 bg-yellow-300">xsd</div>
+    <Button />
+  </div>
+);
 
 export default Home;
---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,5 @@
+export const Button = () => (
+  <div className="bg-slate-700">this is not a button</div>
+);
+
+export default Button;
